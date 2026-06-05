b526df31645c6e4cfe49317bd6c96efb88386649
Esma Hajibaba <esmaekposta@gmail.com>
2026-06-05 10:38:16 +0300

    Yeni özellik eklendi

--- CONFIG/settings.py
+++
@@ -1,3 +0,0 @@
-# config/settings.py
-# Proje genel ayarlarının bulunduğu dosya
-# Dosya yolları, sabit değerler ve yapılandırmalar burada tutulur
--- UI/menu.py
+++
@@ -1,3 +0,0 @@
-# ui/menu.py
-# Kullanıcı arayüzü (CLI menü sistemi)
-# Kullanıcıdan giriş alır ve ilgili service katmanına yönlendirir
